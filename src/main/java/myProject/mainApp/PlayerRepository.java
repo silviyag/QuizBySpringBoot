e1106401a97dc053c14138cbf0782181055b3e67
silviyag <workingonmyretirement@gmail.com>
2021-03-03 13:22:24 +0100

    main small edits

--- src/main/java/myProject/mainApp/PlayerRepository.java
+++ src/main/java/myProject/mainApp/PlayerRepository.java
@@ -1,9 +1,7 @@
 package myProject.mainApp;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-import myProject.Model.Employee;
 import myProject.Model.Player;
 
 public interface PlayerRepository extends MongoRepository<Player, String> {
-	}
+}
