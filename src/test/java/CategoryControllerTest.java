b6f771089a9e96f4decc4252c8558be8c8cede35
silviyag <workingonmyretirement@gmail.com>
2021-03-14 20:16:03 +0100

    main added test method to categorytest class

--- src/test/java/CategoryControllerTest.java
+++ src/test/java/CategoryControllerTest.java
@@ -1,16 +1,12 @@
-import java.awt.PageAttributes.MediaType;
+
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.bind.annotation.GetMapping;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 import myProject.Model.Category;
 import myProject.mainApp.Repos.CategoryRepository;
@@ -19,9 +15,14 @@ import myProject.mainApp.Repos.CategoryRepository;
 @AutoConfigureMockMvc
 public class CategoryControllerTest {
 
-	private Category createRandomCategory() {
-		Category category = new Category();
-		category.setCategoryTitle("Sport");
-		return category;
-	}
+	 @Autowired private CategoryRepository categoryRepository;
+	 
+	   @Test
+	    public void testSaveCategory() {
+
+			Category category = new Category("Sport");
+			categoryRepository.save(category);
+			List<Category> categories = categoryRepository.findAll();
+	        assertNotNull(categories);
+	    }
 }
